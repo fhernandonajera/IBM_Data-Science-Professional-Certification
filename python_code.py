2fe251a1f3bd04f5f36d721f45dbf6b7d22ffe1f
Fernando Najera <63481331+fhernandonajera@users.noreply.github.com>
2020-05-03 17:53:45 -0400

    Update python_code.py

--- python_code.py
+++ python_code.py
@@ -1,2 +1,6 @@
 print("Hello Github!")
+# This is going to be used mainly for the Capstone Project for Applied Data Science with IBM and Coursera
+import pandas as pd
+import numpy as np
+print("Hello Capstone Project Course!")
 
